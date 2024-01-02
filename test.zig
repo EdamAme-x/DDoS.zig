279fc37ed948625c6494d2eb82eb63001004e0d9
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2024-01-02 20:46:32 +0900

    add

---
+++ test.zig
@@ -0,0 +1,9 @@
+const zod = @import("zod.zig");
+const std = @import("std");
+
+test "validate" {
+    try std.testing.expectEqualStrings("google.com", zod.validateURL("https://google.com"));
+    try std.testing.expectEqualStrings("google.com", zod.validateURL("google.com"));
+    try std.testing.expectEqualStrings("google.com", zod.validateURL("http://google.com:8080"));
+    try std.testing.expectEqualStrings("google.com", zod.validateURL("http://google.com:8080/a"));
+}
