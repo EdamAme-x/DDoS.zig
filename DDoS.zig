8ed6ec8b50b76ff7cd96d0b1c0ada31679a0f963
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2024-01-02 20:39:14 +0900

    add

--- DDoS.zig
+++ DDoS.zig
@@ -3,9 +3,16 @@ const logger = @import("logger.zig");
 const builtin = @import("builtin");
 const zod = @import("zod.zig");
 
-pub fn main() void {
+pub fn main() !void {
     logger.log("info", "Welcome to DDoS.zig v1.00 (L7)");
-    const url = "https://a.com:80/asass";
+    const stdin = std.io.getStdIn().reader();
+
+    var inputBuffer: [101]u8 = undefined;
+    std.debug.print("[TARGET URL]: ", .{});
+    const t = try stdin.read(&inputBuffer);
+    std.debug.print("\n", .{});
+
+    const url = inputBuffer[0..t];
 
     logger.log("target", zod.validateURL(url));
 }
