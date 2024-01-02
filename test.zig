dacd2a070ee969a6af14c5a3017e93a3ba2ca5a5
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2024-01-02 20:47:38 +0900

    add

--- test.zig
+++ test.zig
@@ -6,4 +6,6 @@ test "validate" {
     try std.testing.expectEqualStrings("google.com", zod.validateURL("google.com"));
     try std.testing.expectEqualStrings("google.com", zod.validateURL("http://google.com:8080"));
     try std.testing.expectEqualStrings("google.com", zod.validateURL("http://google.com:8080/a"));
+    try std.testing.expectEqualStrings("google.com", zod.validateURL("google.com/"));
+    try std.testing.expectEqualStrings("google.com", zod.validateURL("http://google.com:1111/ax/xa/"));
 }
